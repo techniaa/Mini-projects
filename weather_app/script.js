696ab599d809f901252d3ed529d926fbda5ef3b9
techniaa <nehagc8485@gmail.com>
2025-08-13 15:56:25 +0530

    weather-App

---
+++ weather_app/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded',()=>{
+ const cityInput=document.getElementById('city-input');
+
+ const getWeatherButton=document.getElementById('get-weather-btn');
+ const weatherInfo=document.getElementById('weather-info')
+  const cityNameDisplay=document.getElementById('city-name')
+   const temperatureDisplay=document.getElementById('temperature')
+    const descriptionDisplay=document.getElementById('description')
+     const errorMessage=document.getElementById('error-message') 
+})
